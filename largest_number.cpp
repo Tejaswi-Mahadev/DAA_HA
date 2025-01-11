4a3fc6a89ba4351350988a1636fd8abb318acf0e
Tejaswi-Mahadev <tejaswi054@gmail.com>
2025-01-11 18:12:41 +0530

    Create largest_number.cpp

---
+++ largest_number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        vector<string> numStrs;
+        for (int num : nums) {
+            numStrs.push_back(to_string(num));
+        }
+        sort(numStrs.begin(), numStrs.end(), [](const string& a, const string& b) {
+            return a + b > b + a;
+        });
+        if (numStrs[0] == "0") return "0";
+        string result;
+        for (const string& numStr : numStrs) {
+            result += numStr;
+        }
+        
+        return result;
+    }
+};
