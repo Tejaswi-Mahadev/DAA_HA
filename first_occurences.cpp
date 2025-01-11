cca4595844ea3dcba6c1ccd1f7b73f1086718302
Tejaswi-Mahadev <tejaswi054@gmail.com>
2025-01-11 18:02:09 +0530

    Update first_occurences.cpp

--- first_occurences.cpp
+++ first_occurences.cpp
@@ -1,1 +1,11 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        size_t index = haystack.find(needle);
+        return (index != string::npos) ? static_cast<int>(index) : -1;
+    }
+};
 
